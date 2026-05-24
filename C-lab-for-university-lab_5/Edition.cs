2bcb3b933513f70f34fc197e5ad0dcf48aeffdcc
VelenchukVolodymyr <velenchuk.volodymyr@chnu.edu.ua>
2026-05-24 23:33:06 +0300

    Add files via upload

---
+++ C-lab-for-university-lab_5/Edition.cs
@@ -0,0 +1,129 @@
+using System;
+
+namespace Lab_1
+{
+    public class Edition : IComparable<Edition>
+    {
+        protected string _name = null!;
+        protected DateTime _issueDate;
+        protected int _circulation;
+
+        public Edition(string name, DateTime issueDate, int circulation)
+        {
+            Name = name;
+            IssueDate = issueDate;
+            Circulation = circulation;
+        }
+
+        public Edition() : this("", DateTime.MinValue, 0)
+        {
+        }
+
+        public string Name
+        {
+            get => _name;
+            init => _name = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public DateTime IssueDate
+        {
+            get => _issueDate;
+            init => _issueDate = value;
+        }
+
+        public int Circulation
+        {
+            get => _circulation;
+            init
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), "Circulation cannot be negative");
+                _circulation = value;
+            }
+        }
+
+        public virtual object DeepCopy()
+        {
+            return new Edition(Name, IssueDate, Circulation);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, IssueDate: {IssueDate:d}, Circulation: {Circulation}";
+        }
+
+        public virtual string ToShortString()
+        {
+            return ToString();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is not Edition other)
+                return false;
+
+            return Name == other.Name &&
+                   IssueDate == other.IssueDate &&
+                   Circulation == other.Circulation;
+        }
+
+        public static bool operator ==(Edition? e1, Edition? e2)
+        {
+            if (ReferenceEquals(e1, e2))
+                return true;
+
+            if (e1 is null || e2 is null)
+                return false;
+
+            return e1.Equals(e2);
+        }
+
+        public static bool operator !=(Edition? e1, Edition? e2)
+        {
+            return !(e1 == e2);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, IssueDate, Circulation);
+        }
+
+        public int CompareTo(Edition? other)
+        {
+            if (other == null)
+                return 1;
+
+            return string.Compare(Name, other.Name, StringComparison.CurrentCulture);
+        }
+    }
+
+    public class EditionIssueDateComparer : IComparer<Edition>
+    {
+        public int Compare(Edition? x, Edition? y)
+        {
+            if (ReferenceEquals(x, y))
+                return 0;
+            if (x == null)
+                return -1;
+            if (y == null)
+                return 1;
+
+            return DateTime.Compare(x.IssueDate, y.IssueDate);
+        }
+    }
+
+    public class EditionCirculationComparer : IComparer<Edition>
+    {
+        public int Compare(Edition? x, Edition? y)
+        {
+            if (ReferenceEquals(x, y))
+                return 0;
+            if (x == null)
+                return -1;
+            if (y == null)
+                return 1;
+
+            return x.Circulation.CompareTo(y.Circulation);
+        }
+    }
+}
